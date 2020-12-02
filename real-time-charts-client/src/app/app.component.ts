5c134a3c8d133a27bb581178ca94a45ead995196
Henrik Lauritsen <lauritsen@gmail.com>
2020-12-02 22:06:56 +0100

    getting data pushed from server to client

--- real-time-charts-client/src/app/app.component.ts
+++ real-time-charts-client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { SignalRService } from './services/signal-r.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(public signalRService: SignalRService, private http: HttpClient) {};
+  
+  ngOnInit(): void {
+    this.signalRService.startConnection();
+    this.signalRService.addTransferChartDataListener();
+    this.startHttpRequest();
+  }
+
+  private startHttpRequest = () => {
+    this.http.get('https://localhost:5001/api/chart')
+    .subscribe(res => {
+      console.log(res);
+    })
+  }
   title = 'real-time-charts-client';
 }
