2dc41ec67ad65bcb4f010b857bc3cadb38eac186
Henrik Lauritsen <lauritsen@gmail.com>
2020-12-02 21:44:07 +0100

    adding chart controller and generating fake data

---
+++ RealTimeChartsServer/RealTimeChartsServer/TimerFeatures/TimerManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace RealTimeChartsServer.TimerFeatures
+{
+    public class TimerManager
+    {
+        private Timer _timer;
+        private AutoResetEvent _autoResetEvent;
+        private Action _action;
+
+        public DateTime TimerStarted { get; }
+
+        public TimerManager(Action action)
+        {
+            _action = action;
+            _autoResetEvent = new AutoResetEvent(false);
+            _timer = new Timer(Execute, _autoResetEvent, 1000, 2000);
+            TimerStarted = DateTime.UtcNow;
+        }
+
+        public void Execute(object state)
+        {
+            _action();
+
+            if((DateTime.UtcNow - TimerStarted).Seconds > 60)
+            {
+                _timer.Dispose();
+            }
+        }
+    }
+}
