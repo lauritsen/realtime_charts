4dff5de34ae56805df357936bef5dfe48132dec8
Henrik Lauritsen <lauritsen@gmail.com>
2020-12-02 22:18:14 +0100

    showing chart in frontend

--- real-time-charts-client/src/app/app.component.ts
+++ real-time-charts-client/src/app/app.component.ts
@@ -8,6 +8,21 @@ import { SignalRService } from './services/signal-r.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  public chartOptions: any = {
+    scaleShowVerticalLines: true,
+    responsive: true,
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
+  };
+  public chartLabels: string[] = ['Real time data for the chart'];
+  public chartType: string = 'bar';
+  public chartLegend: boolean = true;
+  public colors: any[] = [{ backgroundColor: '#5491DA'}, { backgroundColor: '#E74C3C' }, { backgroundColor: '#82E0AA' }, { backgroundColor: '#E5E7E9' }];
 
   constructor(public signalRService: SignalRService, private http: HttpClient) {};
   
--- real-time-charts-client/src/app/services/signal-r.service.ts
+++ real-time-charts-client/src/app/services/signal-r.service.ts
@@ -23,7 +23,7 @@ export class SignalRService {
 
   public addTransferChartDataListener = () => {
     this.hubConnection.on('transferchartdata', (data) => {
-      this.data;
+      this.data = data;
       console.log(data);
     })
   }
